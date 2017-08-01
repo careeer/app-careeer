cec1300d246b71417cccd88237953cfa7e6e72f0
Oneximo Gonzalez <oag101@gmail.com>
2017-08-01 00:59:03 -0700

    added staging url

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Careeer
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'http://localhost:8080/', 'https://afternoon-ravine-88728.herokuapp.com/'
         resource(
             '*',
             headers: :any,
