3456342043aa337a2c6e2557c863faa90bddc2a0
Oneximo Gonzalez <oag101@gmail.com>
2017-08-01 01:15:57 -0700

    fixed webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -88,7 +88,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -114,11 +114,12 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         enforce: 'pre',
+        exclude: /node_modules/,
         use: [
           {
             options: {
               formatter: eslintFormatter,
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -165,7 +166,7 @@ module.exports = {
         include: paths.appSrc,
         loader: require.resolve('babel-loader'),
         options: {
-          
+
           compact: true,
         },
       },
