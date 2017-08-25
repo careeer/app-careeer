37f94aed1b6cbdde602e75074a0603ac3d04824e
Oneximo Gonzalez <oag101@gmail.com>
2017-08-25 09:10:44 -0700

    adding key value output

--- src/components/Clients/CareeerVisionInput.jsx
+++ src/components/Clients/CareeerVisionInput.jsx
@@ -12,7 +12,13 @@ const visionStyle = {
 
 @inject('roadmapElements')@observer
 export default class NewClient extends React.Component {
+  state = {
+    key: '',
+  };
+
   handleKeyPress = (event) => {
+    console.log(event.key);
+    this.setState({ key: event.key });
     if (event.key === 'Enter' && this.props.roadmapElements.currentClientVision) {
       this.props.roadmapElements.updateClientVision();
     }
@@ -24,10 +30,11 @@ export default class NewClient extends React.Component {
         style={visionStyle}
         maxLength="160"
         transparent
+        focus
         fluid
         placeholder="what career goal are you focused on achieving? ex: interaction designer at airbnb"
         onKeyPress={this.handleKeyPress}
-        value={this.props.roadmapElements.currentClientVision || ''}
+        value={`${this.props.roadmapElements.currentClientVision} Key: ${this.state.key}` || ''}
         onChange={this.props.roadmapElements.handleClientVisionChange}
       >
         <input style={visionStyle} />
