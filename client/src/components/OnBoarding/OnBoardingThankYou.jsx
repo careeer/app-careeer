340b62ba9f3cf70ea19b5a46eb7b53aeec6c8d29
Oneximo Gonzalez <oag101@gmail.com>
2017-10-09 14:17:10 -0700

    adding thank you back

---
+++ client/src/components/OnBoarding/OnBoardingThankYou.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import ThankYouMessage from './Components/ThankYouMessage';
+
+import './Styles/SimpleOnBoarding.css';
+
+@inject('roadmapElements') @observer
+export default class OnBoardingThankYou extends Component {
+  componentWillMount() {
+    $crisp.push(['do', 'chat:hide']);
+  }
+
+  componentWillUnmount() {
+    $crisp.push(['do', 'chat:show']);
+  }
+
+  render() {
+    this.checkIfNameIsFilled();
+    return (
+      <div className="onBoarding">
+        <ThankYouMessage clientName={this.props.match.params.clientName} />
+      </div>
+    );
+  }
+}
