b5f172de86b2260b5cbca355fe46da47764a0b79
Oneximo Gonzalez <oag101@gmail.com>
2017-10-10 03:45:49 -0700

    fixing default export

--- client/src/components/Constants/CommonElementStyles.js
+++ client/src/components/Constants/CommonElementStyles.js
@@ -1,7 +1,9 @@
-export const COLOR = {
+const COLOR = {
   '#6435c9': 'violet',
   '#e03997': 'pink',
   '#00b5ad': 'teal',
   '#fbbd08': 'yellow',
   transparent: null,
 };
+
+export default COLOR;
