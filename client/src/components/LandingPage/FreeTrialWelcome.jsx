76f8ac4a97f814f16672ef3123453f5ff62bee7c
Oneximo Gonzalez <oag101@gmail.com>
2017-10-11 16:28:22 -0700

    showing crisp on welcome

--- client/src/components/LandingPage/FreeTrialWelcome.jsx
+++ client/src/components/LandingPage/FreeTrialWelcome.jsx
@@ -8,14 +8,6 @@ import SuccessVideo from './SuccessVideo';
 import './Styles/FreeTrialWelcome.css';
 
 export default class FreeTrialWelcome extends Component {
-  componentWillMount() {
-    $crisp.push(['do', 'chat:hide']);
-  }
-
-  componentWillUnmount() {
-    $crisp.push(['do', 'chat:show']);
-  }
-  
   handleClick = () => {
     this.props.history.push('/OnBoarding/Intro');
   };
