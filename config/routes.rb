28e282f9fde10568b1568c51ab9c5a791ad6d92c
Oneximo Gonzalez <oag101@gmail.com>
2017-10-21 08:06:58 -0700

    cleaning routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,5 @@ Rails.application.routes.draw do
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
-  get '/:id' => 'v1/clients#roadmap', defaults: { format: :json }
   post 'update/:id' => 'v1/clients#duplicate', defaults: { format: :json }
 end
