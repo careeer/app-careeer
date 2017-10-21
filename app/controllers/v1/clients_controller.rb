a4bf9ec30bb40056549c5ebd77d780694f352a15
Oneximo Gonzalez <oag101@gmail.com>
2017-10-21 08:53:54 -0700

    adding show back

--- app/controllers/v1/clients_controller.rb
+++ app/controllers/v1/clients_controller.rb
@@ -8,6 +8,12 @@ class V1::ClientsController < ApplicationController
     render :index, status: :ok
   end
 
+  # GET /clients/1
+  # GET /clients/1.json
+  def show
+    render :create, status: :ok
+  end
+
   # POST /clients
   # POST /clients.json
   def create
@@ -15,6 +21,16 @@ class V1::ClientsController < ApplicationController
 
     if @client.save
       render :create, status: :created
+    else
+      render json: @client.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /clients/1
+  # PATCH/PUT /clients/1.json
+  def update
+    if @client.update(client_params)
+      render :create, status: :ok
     else
       render json: @client.errors, status: :unprocessable_entity
     end
@@ -28,16 +44,6 @@ class V1::ClientsController < ApplicationController
       render :duplicate, status: :ok
     else
       render json: @new_client.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /clients/1
-  # PATCH/PUT /clients/1.json
-  def update
-    if @client.update(client_params)
-      head(:ok)
-    else
-      render json: @client.errors, status: :unprocessable_entity
     end
   end
 
