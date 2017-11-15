089ce152f925f6a8355fea07d0850960802829cc
Oneximo Gonzalez <oag101@gmail.com>
2017-11-14 21:30:54 -0800

    removing redirect

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,17 +7,17 @@ import './index.css';
 import stores from './stores';
 import App from './components/App';
 import ScrollToTop from './components/Lib/ScrollToTop';
-import HttpsRedirect from './components/Lib/HttpsRedirect';
+// import HttpsRedirect from './components/Lib/HttpsRedirect';
 
 render(
   <Provider {...stores}>
-    <HttpsRedirect>
-      <BrowserRouter>
-        <ScrollToTop>
-          <App />
-        </ScrollToTop>
-      </BrowserRouter>
-    </HttpsRedirect>
+
+    <BrowserRouter>
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
+    </BrowserRouter>
+
   </Provider>,
   document.getElementById('root'),
 );
