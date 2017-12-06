cdd15a364762ac1f2a4fbb534dfc942ea72556d6
Oneximo Gonzalez <oag101@gmail.com>
2017-12-06 03:00:30 -0800

    adding html emails

--- config/initializers/sparkpost_rails.rb
+++ config/initializers/sparkpost_rails.rb
@@ -1,14 +1,8 @@
 SparkPostRails.configure do |c|
-  # c.sandbox = true                                # default: false
   c.track_opens = true                            # default: false
   c.track_clicks = true                           # default: false
-  # c.return_path = 'BOUNCE-EMAIL@YOUR-DOMAIN.COM'  # default: nil
-  # c.campaign_id = 'YOUR-CAMPAIGN'                 # default: nil
   c.transactional = true                          # default: false
-  # c.ip_pool = "MY-POOL"                           # default: nil
   c.inline_css = true                             # default: false
-  # c.html_content_only = true                      # default: false
-  # c.subaccount = "123"                            # default: nil
-  # c.return_path = 'onex@bounce.careeer.me'
+  c.html_content_only = true                      # default: false
   c.api_key       = ENV['SPARKPOST_API_KEY']
 end
