ee294e55db864aa6d5fe34781183c98da1be7735
Oneximo Gonzalez <oag101@gmail.com>
2017-12-06 00:01:01 -0800

    updating sparkpost config

--- config/initializers/sparkpost_rails.rb
+++ config/initializers/sparkpost_rails.rb
@@ -1,5 +1,6 @@
 SparkPostRails.configure do |c|
   c.api_key       = ENV['SPARKPOST_API_KEY']
+  c.sandbox = false
   c.track_opens = true                            # default: false
   c.track_clicks = true                           # default: false
   c.transactional = true                          # default: false
