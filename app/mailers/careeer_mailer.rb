21e907dfcda31fee303a2000359a9c189762f752
Oneximo Gonzalez <oag101@gmail.com>
2017-12-07 14:32:36 -0800

    adding delay to email

--- app/mailers/careeer_mailer.rb
+++ app/mailers/careeer_mailer.rb
@@ -2,9 +2,9 @@ class CareeerMailer < ApplicationMailer
   default from: 'Careeer.me <support@careeer.me>'
 
   def trial_end(email, client)
-    @name = client.name
+    @name = client.name.split.first
     @url = "https://www.careeer.me/" + client.slug
-    start_time = DateTime.now + 0.hours
+    start_time = DateTime.now + ENV['TRIAL_END_EMAIL_DELAY'].hours
     mail(to: email, subject: 'Well done! You completed your free trial', date: start_time)
   end
 end
