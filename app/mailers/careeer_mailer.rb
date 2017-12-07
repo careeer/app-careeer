9e9f94864f605b3e0c28fee6522edaecafd24550
Oneximo Gonzalez <oag101@gmail.com>
2017-12-07 14:43:26 -0800

    converting delay to number

--- app/mailers/careeer_mailer.rb
+++ app/mailers/careeer_mailer.rb
@@ -4,7 +4,7 @@ class CareeerMailer < ApplicationMailer
   def trial_end(email, client)
     @name = client.name.split.first
     @url = "https://www.careeer.me/" + client.slug
-    start_time = DateTime.now + ENV['TRIAL_END_EMAIL_DELAY'].hours
+    start_time = DateTime.now + ENV['TRIAL_END_EMAIL_DELAY'].to_f.hours
     mail(to: email, subject: 'Well done! You completed your free trial', date: start_time)
   end
 end
