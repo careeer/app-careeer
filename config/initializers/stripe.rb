9f16085577e73b61b196295c4bbd77be10a4649d
Oneximo Gonzalez <oag101@gmail.com>
2018-01-09 23:58:43 -0800

    adding sign in secret

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,5 @@
 Stripe.api_key = Rails.application.secrets.stripe_private_key
+StripeEvent.signing_secret = Rails.application.secrets.stripe_signing_secret
 
 StripeEvent.configure do |events|
   events.subscribe "charge.succeeded", Webhooks::ChargeSucceeded.new
