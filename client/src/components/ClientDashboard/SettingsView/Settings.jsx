a462b78ab6d3deee65893d035d3aa195e7137812
Oneximo Gonzalez <oag101@gmail.com>
2018-01-22 23:06:21 -0800

    adding settings styles and client info

--- client/src/components/ClientDashboard/SettingsView/Settings.jsx
+++ client/src/components/ClientDashboard/SettingsView/Settings.jsx
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import { Route, withRouter } from 'react-router-dom';
-import { Grid, Button, Dimmer } from 'semantic-ui-react';
+import { Grid, Button, Dimmer, Image } from 'semantic-ui-react';
 
 import CloseButton from './CloseButton';
 import CareeerLogo from '../../Lib/CareeerLogo';
@@ -34,6 +34,7 @@ class Settings extends Component {
 
   render() {
     console.log(this.props);
+    const avatarUrl = this.props.roadmapElements.currentClientAvatar || 'https://res.cloudinary.com/careeer/image/upload/v1504959238/Careeer_logo_a3gu5x.png';
     return (
       <Dimmer
         page
@@ -43,6 +44,19 @@ class Settings extends Component {
       >
         <CareeerLogo />
         <CloseButton onCloseClick={this.props.onCloseClick} />
+        <div className="settingsLayout">
+          <div className="messageBody">
+            <h1 className="introHeader">
+              {this.props.roadmapElements.currentClient}!
+            </h1>
+            <Image
+              avatar
+              alt="avatar"
+              className="avatar"
+              src={avatarUrl}
+            />
+          </div>
+        </div>
         <Button
           basic
           content="Sign Out"
