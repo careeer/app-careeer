e5f8f2719a75bc861e1a0d4407208e0906ae0e51
Oneximo Gonzalez <oag101@gmail.com>
2018-01-24 23:33:45 -0800

    adding cancel, change payment, returning card info to rails

--- app/controllers/v1/subscriptions_controller.rb
+++ app/controllers/v1/subscriptions_controller.rb
@@ -4,7 +4,13 @@ module V1
   # Subscription endpoints
   class SubscriptionsController < ApplicationController
     def show
-      render json: { plan: current_user.plan }, status: :ok
+      render json: {
+        plan: current_user.plan,
+        card_last4: current_user.card_last4,
+        card_exp_month: current_user.card_exp_month,
+        card_exp_year: current_user.card_exp_year,
+        card_brand: current_user.card_brand
+      }, status: :ok
     end
 
     def new
@@ -12,6 +18,14 @@ module V1
     end
 
     def upgrade
+      invoice = Stripe::Invoice.create(
+        :customer => "cus_9CQ7bS9zi2gBTP",
+      )
+      invoice.pay
+
+    end
+
+    def downgrade
       invoice = Stripe::Invoice.create(
         :customer => "cus_9CQ7bS9zi2gBTP",
       )
@@ -48,8 +62,45 @@ module V1
       end
     end
 
-    def destroy
+    def update
+      customer =  current_user.stripe_customer
 
+      begin
+        source = customer.sources.create(source: params[:stripeToken])
+
+        customer.default_source = source.id
+        customer.save
+
+        current_user.assign_attributes(
+          card_last4: params[:last4],
+          card_brand: params[:card_type],
+          card_exp_month: params[:exp_month],
+          card_exp_year: params[:exp_year]
+        )
+
+        if current_user.save
+          head(:ok)
+        else
+          head(:unprocessable_entity)
+        end
+
+      rescue Stripe::CardError => e
+        render json: { error: e.message}, status: :bad_request
+
+      end
+
+    end
+
+    def destroy
+      customer =  current_user.stripe_customer
+      subscription = customer.subscription.retrieve(current_user.stripe_subscription_id).delete
+
+      expires_at = Time.zone.at(subscription.current_period_end)
+      if current_user.update(expires_at: expires_at, stripe_subscription_id: nil)
+        head(:ok)
+      else
+        head(:unprocessable_entity)
+      end
     end
 
     private
