0269ef3b4c024817ebf196ef729ec19d1eb1112e
Oneximo Gonzalez <oag101@gmail.com>
2018-02-07 20:24:01 -0800

    send email for recurrent subscription failed payments

--- app/models/webhooks/payment_failed.rb
+++ app/models/webhooks/payment_failed.rb
@@ -3,17 +3,19 @@ module Webhooks
     def call(event)
       invoice = event.data.object
 
-      user = User.find_by(stripe_id: invoice.customer)
-      subscription = Stripe::Subscription.retrieve(user.stripe_subscription_id)
+      if invoice.lines.total_count == 1
+        user = User.find_by(stripe_id: invoice.customer)
+        subscription = Stripe::Subscription.retrieve(user.stripe_subscription_id)
 
-      CareeerMailer.payment_failed(
-        user.email,
-        user.clients.first,
-        subscription.plan.name,
-        (invoice.amount_due / 100.00),
-        user.card_last4
-      ).deliver
-      user.update(subscription_status: "pending")
+        CareeerMailer.payment_failed(
+          user.email,
+          user.clients.first,
+          subscription.plan.name,
+          (invoice.amount_due / 100.00),
+          user.card_last4
+        ).deliver
+        user.update(subscription_status: "pending")
+      end
     end
   end
 end
