cf81b0b352134a31a3a81e6696bfd0c3af886c9d
Oneximo Gonzalez <oag101@gmail.com>
2018-04-19 13:34:24 -0700

    clean up footer

--- client/src/components/Homepage/Components/Footer.jsx
+++ client/src/components/Homepage/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Icon, Divider } from 'semantic-ui-react';
+import { Grid, Icon } from 'semantic-ui-react';
 
 const Footer = () => (
   <Grid>
