ba493ac16f5c03da2be35ba5837bc2e2b93b3e27
Oneximo Gonzalez <oag101@gmail.com>
2018-07-26 00:56:30 -0400

    updating homepage

--- client/src/components/Homepage/Components/MissionSection.jsx
+++ client/src/components/Homepage/Components/MissionSection.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 const MissionSection = () => (
   <section className="mission">
     <h1>
-      Work with a coach to pitch your transferable skills, rebrand
-      your resume, and create a job search strategy that lands you
-      the job you want
+      Learn to pitch your transferable skills, rebrand
+      your resume, and land job you want
     </h1>
   </section>
 );
