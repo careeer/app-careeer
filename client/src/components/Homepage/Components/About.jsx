df27f0a5f49f2357628cc49e6b197f3f907b31bd
oag101 <onex.gonzalez@accenture.com>
2019-02-27 10:08:55 -0600

    updating about section

--- client/src/components/Homepage/Components/About.jsx
+++ client/src/components/Homepage/Components/About.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 
 import LightbulbsImage from '../../Icons/LightbulbsImage';
 import HiBubbleImage from '../../Icons/HiBubbleImage';
-import Anya from '../../Icons/Images/Anya.png';
-import Tiffany from '../../Icons/Images/Tiffany.png';
-import Onex from '../../Icons/Images/Onex.png';
 
 const About = () => (
   <div className="aboutPage">
@@ -17,18 +14,18 @@ const About = () => (
       <div className="greenRectangle" />
       <article className="ourVision">
         <h1>
-          <span className="greenInlineRectangle">Our vision</span> is to build a
-          bridge to jobs in tech
+          <span className="greenInlineRectangle">Our vision</span> is to build a bridge to jobs in
+          tech
         </h1>
         <section>
           <p>
-            At Careeer.me, we know what it’s like to struggle through the job
-            search process. We have experienced the process ourselves, and found
-            that it was like trudging through mud. Up a mountain. With rain.
+            At Careeer.me, we know what it’s like to struggle through the job search process. We
+            have experienced the process ourselves, and found that it was like trudging through mud.
+            Up a mountain. With rain.
           </p>
           <p>
-            Having coached hundreds of working professionals as they look for
-            their first (or 5th) job in tech, we’ve learned a few things:
+            Having coached hundreds of working professionals as they look for their first (or 5th)
+            job in tech, we’ve learned a few things:
           </p>
           <ul>
             <li>
@@ -41,17 +38,14 @@ const About = () => (
             </li>
             <li>
               <h3>3. Accountability</h3>
-              <p>
-                “I get bored/sidetracked/stop caring/apathetic about the job
-                search process”
-              </p>
+              <p>“I get bored/sidetracked/stop caring/apathetic about the job search process”</p>
             </li>
           </ul>
           <p>
-            We’ve found that direction, mentorship, and custom skill development
-            are critical to creating a successful job search strategy. We built
-            Careeer.me to provide a clear path to jobs in tech, while solving
-            the trending issues in communication and interviewing skills.
+            We’ve found that direction, mentorship, and custom skill development are critical to
+            creating a successful job search strategy. We built Careeer.me to provide a clear path
+            to jobs in tech, while solving the trending issues in communication and interviewing
+            skills.
           </p>
           <p>
             You can read more about why we started Careeer.me &nbsp;
@@ -66,39 +60,60 @@ const About = () => (
         </section>
       </article>
       <header className="aboutTitle">
-        <h1>Our Team</h1>
+        <h1>Our Coaches</h1>
       </header>
       <section className="ourTeam">
-        <figure>
-          <a href="https://www.linkedin.com/in/anya-iverova-27046822/">
-            <img src={Anya} alt="Anya" />
-            <figcaption>
-              Anya Iverova
-              <br />
-              CEO/Coach/Founder
-            </figcaption>
-          </a>
-        </figure>
-        <figure>
-          <a href="https://www.linkedin.com/in/tiffanyestrella">
-            <img src={Tiffany} alt="Tiffany" />
-            <figcaption>
-              Tiffany Estrella
-              <br />
-              UX/UI Designer
-            </figcaption>
-          </a>
-        </figure>
-        <figure>
-          <a href="https://www.linkedin.com/in/onex-g/">
-            <img src={Onex} alt="Onex" />
-            <figcaption>
-              Onex Gonzalez
-              <br />
-              App Developer
-            </figcaption>
-          </a>
-        </figure>
+        <p>
+          You will work with coaches who are experts in their field and have direct professional
+          experience as product managers, UX designers, software engineers, project managers, sales
+          professionals, and more. We understand how to review portfolios, the difference between
+          agile and scrum, and why you used MEAN stack to build your product.
+        </p>
+        <p>
+          Careeer.me’s primary coach is the CEO and Founder, Anya Iverova. Here are highlight’s from
+          Anya’s career path:
+        </p>
+        <ul>
+          <li>8 years in the education industry</li>
+          <br />
+          <li>
+            Started out in university partnership development and corporate business development
+          </li>
+          <br />
+          <li>PM instructor at General Assembly</li>
+          <br />
+          <li>
+            Launched Fresh Conceptual, product design studio and worked with clients such as
+            DMV.ORG, City of West Hollywood, and more
+          </li>
+          <br />
+          <li>
+            Launched Careeer.me to provide high quality career support for current tech
+            professionals and newbies gaining traction in the field.
+          </li>
+        </ul>
+      </section>
+      <header className="aboutTitle">
+        <h1>What you can expect from our culture:</h1>
+      </header>
+      <section className="ourTeam">
+        <p>
+          <strong>Efficiency</strong> - we work with a sense of urgency to get you into your next
+          job, fast.
+        </p>
+        <br />
+        <p>
+          <strong>Personability</strong> - No robots here, but rather, smart people who listen
+          deeply, genuinely appreciate your business, and gameplan with you to get you where you
+          want to be.
+        </p>
+        <br />
+        <p>
+          <strong>Empathy</strong> - If you know exactly what you want, we’re right there with you.
+          If you’re not sure, we’ll do everything we can to assess the situation and figure it out
+          together. And if you’re feeling depressed, insecure, or otherwise crappy, we understand
+          and will meet you where you’re at.
+        </p>
       </section>
     </div>
   </div>
